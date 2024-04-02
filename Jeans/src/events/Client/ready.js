bca926a2114dda76e9e60ae50a9fca0016d61e49
matbiecc <154546910+matbiecc@users.noreply.github.com>
2024-04-02 14:01:58 +0700

    Delete Jeans directory

--- Jeans/src/events/Client/ready.js
+++
@@ -1,9 +0,0 @@
-const colors = require('colors');
-const log = require('../../utils/logger');
-module.exports = {
-    name: 'ready',
-    isOnce: true,
-    run: async (client) => {
-        log.success(`${client.user.tag} is up and ready to go.`);
-    },
-};
--- Jeans/src/events/Guild/guildDelete.js
+++
@@ -1,8 +0,0 @@
-const report = require('../../utils/report.js');
-const log = require('../../utils/logger.js');
-module.exports = {
-    name: 'guildDelete',
-    run: async (client, guild) => {
-        report.guild(guild, client.shard.ids[0], 'leave');
-    },
-};
